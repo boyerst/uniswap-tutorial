c1c66f95268fa9e24746f2b7c230bcc6d62e9b9c
Steve Boyer <stephen.a.boyer@gmail.com>
2022-06-08 14:31:33 -0500

    Provide explanation for useQuery logic and syntax

--- src/components/App.js
+++ src/components/App.js
@@ -8,16 +8,19 @@ import gql from 'graphql-tag'
 import uniswapLogo from '../uniswap-logo.png'
 import daiLogo from '../dai-logo.png'
 
+// The client is imported into index.js to establish a connection to the ApolloProvider which allows us to talk to The Graph
 export const client = new ApolloClient({
   link: new HttpLink({
     uri: 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2'
   }),
   fetchOptions: {
+    // We use no-cors mode for the request
     mode: 'no-cors'
   },
   cache: new InMemoryCache()
 })
 
+// DEFINE THE QUERIES
 const DAI_QUERY = gql`
   query tokens($tokenAddress: Bytes!) {
     tokens(where: { id: $tokenAddress }) {
@@ -36,7 +39,13 @@ const ETH_PRICE_QUERY = gql`
 `
 
 function App() {
+
+  // MAKE THE QUERIES
   // Define vars using useQuery so we can make Apollo queries with React using React hooks
+  // Whenever the App component renders, the useQuery hook automatically executes our query and returns a destructured object containing loading, error, and data properties
+    // We unpack these properties from the object and assign them to new variable names
+        // As long as loading (ethLoading/daiLoading) is true, the query is still in flight
+        // When loading is no longer true, the query has completed and our component will render 
   const { loading: ethLoading, data: ethPriceData } = useQuery(ETH_PRICE_QUERY)
   const { loading: daiLoading, data: daiData } = useQuery(DAI_QUERY, {
     // Pass in the vars that our query requires - in this case the DAI mainnet contract address
@@ -44,6 +53,8 @@ function App() {
       tokenAddress: '0x6b175474e89094c44da98b954eedeac495271d0f'
     }
   })
+
+  // We format the data we get back from the queries
   // We use the Logical AND operator to render something or nothing
     // The constant is defined by data returned by the API queries
     // Evaluating from L to R, we'll either receive the value of the first falsy encountered OR if all values are truthy we will receive the last of the last operand
